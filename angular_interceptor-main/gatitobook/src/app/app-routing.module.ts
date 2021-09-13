cf0e9578e87adca25e6409246cec0e11c03dde92
thuanedesouza <36061136+thuanedesouza@users.noreply.github.com>
2021-09-12 22:01:51 -0300

    Guarda de Rota para quando o usuário estivar logado
    controlando fluxo do usuário com guarda de rota

--- angular_interceptor-main/gatitobook/src/app/app-routing.module.ts
+++ angular_interceptor-main/gatitobook/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AutenticacaoGuard } from './autenticacao/autenticacao.guard';
+import { LoginGuard } from './autenticacao/login.guard';
 
 const routes: Routes = [
   {
@@ -11,6 +12,7 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+    canLoad:[LoginGuard]
   },
   {
     path: 'animais',
