dfcfd14a9b6944f8fff1ed7d8e903966ceffdbe4
thuanedesouza <36061136+thuanedesouza@users.noreply.github.com>
2021-09-21 18:38:56 -0300

    registrando interceptor

--- angular_interceptor-main/gatitobook/src/app/animais/animais.service.ts
+++ angular_interceptor-main/gatitobook/src/app/animais/animais.service.ts
@@ -15,17 +15,23 @@ export class AnimaisService {
   constructor(private http: HttpClient, private TokenService:TokenService) { }
 
   listaDoUsuario(nomeDoUsuario:string): Observable<Animais>{
+    /*
+    Com o interceptor isso já vai acontecer sem ter que declarar nada
     const token = this.TokenService.retornaToken();
     const headers = new HttpHeaders().append('x-access-token',token);
 
-    //fazendo get passando o token que o backend exige no headers
-    return this.http.get<Animais>(`${API}/${nomeDoUsuario}/photos`, {headers});
+    fazendo get passando o token que o backend exige no headers*/
+    return this.http.get<Animais>(`${API}/${nomeDoUsuario}/photos`);
   }
 
 
   buscaPorID(id:number):Observable<Animal>{
+    /*
+    Substituido pelo interceptor
+
     const token = this.TokenService.retornaToken();
-    const headers = new HttpHeaders().append('x-access-token', token);
-    return this.http.get<Animal> (`${API}/photos/${id}`, {headers});
+    const headers = new HttpHeaders().append('x-access-token', token);*/
+
+    return this.http.get<Animal> (`${API}/photos/${id}`);
   }
 }
